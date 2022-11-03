e27a940c3761da16bffde9459a1f72fbb66cc49b
Acuzou <103382319+Acuzou@users.noreply.github.com>
2022-11-03 06:48:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,44 +22,66 @@ import {
 
  //Connection with our firebase page base on apiKey
  const firebaseConfig = {
-    apiKey: "AIzaSyAqpi_Ngce7eIpiVjYjq3-Kra58mXSSe2I",
-    authDomain: "lumos-4786c.firebaseapp.com",
-    projectId: "lumos-4786c",
-    storageBucket: "lumos-4786c.appspot.com",
-    messagingSenderId: "1048784963409",
-    appId: "1:1048784963409:web:bd290641e2a69b9c4f7570",
-	 measurementId: "G-KZZDY7FP1E"
+ 	apiKey: "AIzaSyAqpi_Ngce7eIpiVjYjq3-Kra58mXSSe2I",
+    	authDomain: "lumos-4786c.firebaseapp.com",
+    	projectId: "lumos-4786c",
+   	storageBucket: "lumos-4786c.appspot.com",
+    	messagingSenderId: "1048784963409",
+    	appId: "1:1048784963409:web:bd290641e2a69b9c4f7570",
+	measurementId: "G-KZZDY7FP1E"
  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 		
-//Detect auth state
-onAuthStateChanged(auth, user => { 
-    if(user != null){
-      console.log("Connexion user");
-    } else { 
-      console.log("Pas de user");
-    }
-});
-
 //connectAuthEmulator(auth, "http://localhost:9899");
 
 const loginEmailPassword = async() => { 
-	const loginEmail = txtEmail.value;
-	const loginPassword = txtPassword.value;
+  const loginEmail = txtEmail.value;
+  const loginPassword = txtPassword.value;
 	
-	try{
-		const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-		console.log(userCredential.user);
-	} catch(error){
-		console.log(error)
-		showLoginError(error)
+  try{
+    const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+    console.log(userCredential.user);
+  } catch(error){
+      console.log(error)
+      showLoginError(error)
 	}
 	
 }
 
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `Vous n'êtes pas connectés !`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
 btnLogin.addEvenListener("click", loginEmailPassword);
+btnLogout.addEvenListener("click", logout);
+
+const auth = getAuth(firebaseApp);
+monitorAuthState();
+
+
+
+
+
