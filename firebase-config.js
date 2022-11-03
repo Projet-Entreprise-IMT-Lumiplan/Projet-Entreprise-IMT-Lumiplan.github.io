13710aa138a28d92488af701673d41717612685c
Acuzou <103382319+Acuzou@users.noreply.github.com>
2022-11-03 05:31:43 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,33 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// Import the functions we need from firebase
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
+	import { getFirestore } from "firebase/firestore";
+	import { getAuth, onAuthStateChanged } from "firebase/auth";
+	
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+  //Connection with our firebase page base on apiKey
+ 	const firebaseConfig = {
+    	apiKey: "AIzaSyAqpi_Ngce7eIpiVjYjq3-Kra58mXSSe2I",
+    	authDomain: "lumos-4786c.firebaseapp.com",
+    	projectId: "lumos-4786c",
+    	storageBucket: "lumos-4786c.appspot.com",
+    	messagingSenderId: "1048784963409",
+    	appId: "1:1048784963409:web:bd290641e2a69b9c4f7570",
+	  measurementId: "G-KZZDY7FP1E"
+  	};
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+	const auth = getAuth(firebaseApp);
+	const db = getFirestore(app);
+  const analytics = getAnalytics(app);
+		
+	//Detect auth state
+  onAuthStateChanged(auth, user => { 
+    if(user != null){
+      console.log("Connexion user");
+    } else { 
+      console.log("Pas de user");
+    }
+  });
