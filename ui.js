214d65fa71f006b8b1f3857eb220c60e859494b5
Acuzou <103382319+Acuzou@users.noreply.github.com>
2022-11-03 06:31:51 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -4,7 +4,6 @@ export const txtEmail = document.querySelector('#txtEmail')
 export const txtPassword = document.querySelector('#txtPassword')
 
 export const btnLogin = document.querySelector('#btnLogin')
-export const btnSignup = document.querySelector('#btnSignup')
 
 export const btnLogout = document.querySelector('#btnLogout')
 
