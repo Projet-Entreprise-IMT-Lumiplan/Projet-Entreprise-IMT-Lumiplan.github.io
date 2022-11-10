e6b7197360701054505f7911ab4f8223745b4c09
Twane70 <tonduantoine@gmail.com>
2022-11-10 12:02:41 +0100

    contenu modal

--- Logiciels/EXEMPLES copy.js
+++ Logiciels/EXEMPLES copy.js
@@ -15,6 +15,7 @@ for (let i = 0; i < logiciels.length; i++) {
 	pieceElement.setAttribute('class', "btn btn-light article");
     pieceElement.setAttribute('data-toggle' , "modal");
     pieceElement.setAttribute('data-target' , "#exampleModal");
+	pieceElement.setAttribute('data-logiciel', logiciels[i].Identifiant)
 
 	// On crée l’élément img.
 	const imageElement = document.createElement("img");
@@ -65,30 +66,28 @@ boutonTrier.addEventListener("click", function () {
 	console.log(logicielsReordonnees);
 }); */
 
-function addItemsToList(courseName, institute) {
-      var ul = document.getElementById("Certificates-List");
+$('#exampleModal').on('show.bs.modal', function (event) {
+	var button = $(event.relatedTarget) // Button that triggered the modal
+	var index = button.data('logiciel')-1 // Extract info from data-* attributes
+	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	var modal = $(this)
+	modal.find('.modal-title').text(logiciels[index].Outils)
 
-      var _courseName = document.createElement("li");
-      var _institute = document.createElement("li");
+	const newBody = document.createElement("div");
+	for (const [key, value] of Object.entries(logiciels[index])) {
+		
+		//item.innerText = logiciels[index][i];
+		const item = document.createElement("div");
+		item.innerText= key + " : " + value;
+		newBody.append(item)
+		
+	}
+	console.log(newBody)
+	const oldBody = document.getElementsByClassName('modal-body')[0];
+	oldBody.innerHTML='';
+	oldBody.appendChild(newBody);
+	//modal.find('.modal-body input').val(recipient)
+  })
 
-      _courseName.innerHTML = "CourseName: " + courseName;
-      _institute.innerHTML = "Institute: " + institute;
 
-      ul.appendChild(_courseName);
-      ul.appendChild(_institute);
- };
-
-function FetchAllData() {
-      firebase
-        .database()
-        .ref("Certificates-List")
-        .once("value", function (snapshot) {
-          snapshot.forEach(function (ChildSnapshot) {
-            let courseName = ChildSnapshot.val().courseName;
-            let institute = ChildSnapshot.val().institute;
-            addItemsToList(courseName, institute);
-          });
-        });
- };
-
-//window.onload(FetchAllData());
