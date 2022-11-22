4e2af93d2a6c495b35767b35f20bf402ff9ad9c9
Acuzou <103382319+Acuzou@users.noreply.github.com>
2022-11-22 14:35:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Import the functions we need from firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+//import { getFirestore } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
 import { getAuth} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import { getDatabase, ref, set, child, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js";
 	
 // https://firebase.google.com/docs/web/setup#available-libraries
 //Connection with our firebase page base on apiKey
@@ -20,11 +21,12 @@ import { getAuth} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+export const db = getDatabase();
+//export const db = getFirestore(app);
 		
 
 /*
-c
+
 
 
 // Log out
