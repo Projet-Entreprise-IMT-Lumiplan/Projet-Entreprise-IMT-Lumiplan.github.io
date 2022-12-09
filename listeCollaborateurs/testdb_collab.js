97cfaed402cd9d883057becf11fffad2acac3871
NaoT38 <114235778+NaoT38@users.noreply.github.com>
2022-12-09 12:07:04 +0100

    Update testdb_collab.js

--- listeCollaborateurs/testdb_collab.js
+++ listeCollaborateurs/testdb_collab.js
@@ -3,8 +3,9 @@ import {db} from "../index.js"
 const firebaseRef = ref(db, "dataEmployesOutils");
 get(firebaseRef, function(snapshot) {
   snapshot.forEach(function(element) { //element.value.prenom pour aller chercher le prenom
-    console.log(element);
+    console.log(element.val()["Prenom"]);
   })
 });
 
 // $ : toute valeur devient un string
+// espace :
