a089a31fa534dfade78bafe2732adddecde51312
euge44 <eugenie.laugier@gmail.com>
2022-12-15 01:01:48 +0100

    creation comptes

--- public/authentification/AuthBackend.js
+++ public/authentification/AuthBackend.js
@@ -5,14 +5,9 @@ import {auth,db} from '../index.js';
 const txtEmail = document.querySelector('#txtEmail')  
 const txtPassword = document.querySelector('#txtPassword')
 const signInButton = document.querySelector('#signInButton')
-const divAuthState = document.querySelector('#divAuthState')
 const lblAuthState = document.querySelector('#lblAuthState')
-const divLoginError = document.querySelector('#divLoginError')
-const lblLoginErrorMessage = document.querySelector('#lblLoginErrorMessage')
 
-//export var user;
-
-
+//login de l'utilisateur
 const loginEmailPassword = async() => { 
   
   const loginEmail = txtEmail.value;
@@ -20,34 +15,16 @@ const loginEmailPassword = async() => {
 	
   try{
     const userCredential = await signInWithEmailAndPassword(auth,loginEmail, loginPassword);
-    //redirection towards the correct folder
+    //redirection vers la page de logiciels
     window.location.replace("./Logiciels/index.html");
-    /*
-    //creation of the user in the database
-    var user_data={
-      email:txtEmail
-    }
-
-    //push to firebase
-    dbRef.child('users/' + user.uid )
-    */
   }catch(error){
       console.log(error);
       showLoginError(error);
 	}
 }
 
+//listener boutton log in 
 signInButton.addEventListener('click', loginEmailPassword);
-
-
-function getUser(){
-  const subscribe = onAuthStateChanged(auth, (currentUser) => {
-      var user=currentUser;
-      console.log(user);
-  })
-  return(subscribe);
-}
-getUser();
 
 export const showLoginState = (user) => {
   lblAuthState.innerHTML = `You're logged in as (uid: ${user.uid}, email: ${user.email}) `
--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 // Import the functions we need from firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
-import { getAuth} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getDatabase, ref, set, child, update, remove, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-	
+import { getAuth, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 //Connection with our firebase page base on apiKey
- const firebaseConfig = {
-	apiKey: "AIzaSyAqpi_Ngce7eIpiVjYjq3-Kra58mXSSe2I",
+const firebaseConfig = {
+    apiKey: "AIzaSyAqpi_Ngce7eIpiVjYjq3-Kra58mXSSe2I",
 
     authDomain: "lumos-4786c.firebaseapp.com",
 
@@ -22,7 +22,7 @@ import { getDatabase, ref, set, child, update, remove, onValue } from "https://w
     appId: "1:1048784963409:web:bd290641e2a69b9c4f7570",
 
     measurementId: "G-KZZDY7FP1E"
- };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,7 +31,27 @@ export const db = getDatabase(app);
 export const auth = getAuth(app);
 
 
-
-
-
-
+/*
+const dbRef = ref(db)
+function createUsers() {
+    get(child(dbRef, 'dataEmployesOutils')).then((snapshot) => {
+        //console.log(email)
+        snapshot.forEach((idEmploye) => {
+            const email= idEmploye.child('AdresseMail').val();
+    
+            const password = "123456789" //mot de passe par défaut
+            createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+    })
+    .catch((error)=> {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    
+    });;
+            signOut(auth);
+        });
+    }); 
+}
+createUsers()
+*/
